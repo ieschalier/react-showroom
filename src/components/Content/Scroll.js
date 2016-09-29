757a1735381df4496552cab943185d7691f4e09c
igor13127 <igor.eschalier@gmail.com>
2016-09-29 15:57:27 +0200

    Add infinite scroll sample

--- src/components/Content/Scroll.js
+++ src/components/Content/Scroll.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Highlight from 'react-highlight'
+import InfiniteScroll from 'react-stateless-infinite-scroll'
+import faker from 'faker'
 
 const style = {
   container: {
@@ -39,18 +41,110 @@ const style = {
 }
 
 
-const snippet = ``
+const snippet = `import React from 'react'
+import InfiniteScroll from 'react-stateless-infinite-scroll'
 
-const Preview = () => {
+const Scroll = (props) => {
+
+  const Items = props.phrases.map((phrase) => <div>{phrase}</div>)
+
+  return (
+    <InfiniteScroll
+      height={400}
+      items={Items}
+      loadMore={props.actions.loadMore}
+      hasMore={props.hasMore}
+      loading={props.loading}
+      error={props.error}
+    />
+  )
+}
+`
+
+class Preview extends React.Component {
+
+  generateData = () => {
+    const data = []
+    for (var i = 0; i < 50; i++) {
+      data.push(faker.hacker.phrase())
+    }
+
+    return data
+  }
+
+  state = {
+    array: this.generateData(),
+    hasMore: true,
+    loading: false,
+    error: false,
+  }
+
+  loadMore = () => { this.setState({ array: [...this.state.array, ...this.generateData()] }) }
+
+  resetData = () => { this.setState({ array: this.generateData() }) }
+
+  hasMore = (e) => { this.setState({ hasMore: e.target.checked }) }
+
+  loading = (e) => { this.setState({ loading: e.target.checked }) }
+
+  error = (e) => { this.setState({ error: e.target.checked }) }
+
+  render = () => (
+    <div>
+      <button onClick={this.resetData} style={{margin: '0 10px 10px 10px'}}>Reset</button>
+      <input
+        type='checkbox'
+        id='hasMore'
+        defaultChecked={this.state.hasMore}
+        onClick={this.hasMore}
+        style={{margin: '0 10px 10px 10px'}}
+      />
+      <label htmlFor='hasMore'>Has more data</label>
+      <input
+        type='checkbox'
+        id='loading'
+        defaultChecked={this.state.loading}
+        onClick={this.loading}
+        style={{margin: '0 10px 10px 10px'}}
+      />
+      <label htmlFor='loading'>Loading</label>
+      <input
+        type='checkbox'
+        id='error'
+        defaultChecked={this.state.error}
+        onClick={this.error}
+        style={{margin: '0 10px 10px 10px'}}
+      />
+      <label htmlFor='error'>Error</label>
+      <InfiniteScroll
+        height={400}
+        items={this.state.array.map((item, index) => <Cell key={index} content={item}/>)}
+        loadMore={this.loadMore}
+        hasMore={this.state.hasMore}
+        loading={this.state.loading}
+        error={this.state.error}
+      />
+    </div>
+  )
+}
+
+const Cell = ({ content }) => {
+
   const style = {
-    color: 'white',
-    fontFamily: 'Sans-serif',
-    textShadow: '0 0 2px rgba(0,0,0,0.5)',
-    padding: 20
+    paddingTop: 19,
+    boxSizing: 'border-box',
+    height: 60,
+    fontSize: 20,
+    fontWeight: 400,
+    paddingLeft: 15,
+    color: '#666',
+    backgroundColor: '#fbfbfb',
+    borderBottom: '1px solid #eee',
+    cursor: 'pointer',
   }
 
   return (
-    <div></div>
+    <div {...{style}}>{content}</div>
   )
 }
 
